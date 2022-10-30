78ac5cf4e3f781c9cb4b034fd666c9575c25e6ce
Sapphire2260 <116996068+Sapphire2260@users.noreply.github.com>
2022-10-30 11:27:14 +0500

    Add files via upload

---
+++ One.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class One : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Выводит "крикетс" каждый кадр. Хочу чай.
+    void Update()
+    {
+        print("crickets");
+    }
+}
